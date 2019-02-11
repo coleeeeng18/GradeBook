a5d4d6578bad886d137c96ed5f803522f54508c5
cols1101 <coleenjade.ligasan@letran.edu.ph>
2019-02-11 16:38:40 +0800

    Grades First Commit

---
+++ Grades/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine("The Average Grade is: " + stats.AverageGrade);
+            Console.WriteLine("The Highest Grade is: " + stats.HighestGrade);
+            Console.WriteLine("The Lowest Grade is: " + stats.LowestGrade);
+
+        }
+    }
+}
